501a519cf6425c11b67b223be24c51d6a96a3714
drewcornatzer <acornatzer@mail.csuchico.edu>
2018-10-22 17:26:39 -0700

    Red Picture

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include "Bitmap/Bitmap.h"
+#include "Bitmap/bitmap.h"
 #include <iostream>
 #include <vector>
 
@@ -8,6 +8,41 @@ using namespace std;
 //argv - each argument word, stored in an array
 int main(int argc, char* argv[])
 {
-  
+        string filename;
+        Bitmap image;
+        if(argc>2||argc<2)
+         {
+           cout<<"Please specify one image file!\n";
+         }
+        else if(argc==2)
+         {
+           filename=argv[1];
+           image.open(filename);
+           if(image.isImage()==0)
+            {
+                cout<<"Image file must be a bitmap with 24-bit color depth. \n";
+            }
+           else if(image.isImage()==1)
+            {
+                vector <vector <Pixel> > imagePixels= image.toPixelMatrix();
+                cout<<filename<<" is "<<imagePixels.size()<<" pixels high and "<<imagePixels[0].size()<<" pixels wide.\n";
+        for(int row=0;row<imagePixels.size();row++)
+        {
+               
+
+                for(int column=0;column<imagePixels[row].size();column++)
+                {
+                        Pixel color;
+                        color = imagePixels[row][column];
+                        color.green=0;
+                        color.blue=0;
+                        imagePixels[row][column]=color;
+                }
+        image.fromPixelMatrix( imagePixels );
+        image.save("redness.bmp");
+            }
+            }
+
+        }
   return 0;
 }
